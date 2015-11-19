43bba93b2e30509a560c7baf282bcc5699719ceb
Anders Holmgren <aholmgren@atlassian.com>
2015-11-19 17:56:25 +1100

    add value property

--- lib/src/react/react_element.dart
+++ lib/src/react/react_element.dart
@@ -8,24 +8,27 @@ class ReactElement {}
 class Props {
   external bool get disabled;
   external String get className;
+  external String get value;
   external Function get onClick;
   external Function get onChange;
 
-  external factory Props({String className, bool disabled, Function onClick,
-    Function onChange});
+  external factory Props({String className, bool disabled, String value,
+    Function onClick, Function onChange});
 }
 
-Props _props({String className, bool disabled, void onClick(EventHandler<SyntheticMouseEvent> event),
+Props _props({String className, bool disabled, String value,
+    void onClick(EventHandler<SyntheticMouseEvent> event),
     void onChange(EventHandler<SyntheticEvent> event)}) {
-  return new Props(className: className, disabled: disabled, onClick: _wrapEventHandler(onClick),
+  return new Props(className: className, disabled: disabled, value: value,
+      onClick: _wrapEventHandler(onClick),
       onChange: allowInterop(onChange));
 }
 
 ReactElement createElement(typeOrClass, [Map props, children]) {
   var interopProps = new Props();
   if (props != null) {
-    interopProps = _props(className: props["className"], onClick: props["onClick"],
-        onChange: props["onChange"]);
+    interopProps = _props(className: props["className"], value: props["value"],
+        onClick: props["onClick"], onChange: props["onChange"]);
   }
   return internal.createElement(typeOrClass, interopProps, children);
 }
